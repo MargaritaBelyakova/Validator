886ae441b3a0bb8ed63ac26d08c315540457d266
Margo_Belaya <beliakova.rita2016@yandex.ru>
2022-06-16 10:01:42 +0700

    function validateSnils: write a validator of a data's type (number or string)

--- js/validator_snils.js
+++ js/validator_snils.js
@@ -1,5 +1,10 @@
 /*validator_snils.js for tests*/
 
 function validateSnils(snils, error) {
-
+    var result = false;
+    if (typeof snils === 'number') {
+        snils = snils.toString();
+    } else if (typeof snils !== 'string') {
+        snils = '';
+    }
 }
